7a208376adbced9fa0bae728a48c7db6c7bb0e90
Roshan Ravichandran <124917893+roshdezign@users.noreply.github.com>
2023-02-10 01:41:31 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Preloader from "./components/Preloader/Preloader";
+import Timer from "./components/Countdown/Timer";
+import Optin from "./components/Optin/Optin";
+
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>
+          Website
+          <br />
+          Coming Soon
+        </h1>
+        <Optin />
+        <Preloader />
+      </div>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
